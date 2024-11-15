557cf6a5fb29bcd9a987c65d9266eb885701de60
alankritvyas21 <av3h.vyas@gmail.com>
2024-11-15 18:42:16 +0530

    Cros fix

--- src/main/java/com/alankrit/Ecomm/controller/CategoryController.java
+++ src/main/java/com/alankrit/Ecomm/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.alankrit.Ecomm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,26 +23,31 @@ public class CategoryController {
     @Autowired
     categoryService categoryService;
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/category")
 	public List<categoryModel> getCategories() {
 		return categoryService.getAllCategories();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/category/{id}")
 	public categoryModel getCategory(@PathVariable Integer id) {
 		return categoryService.getCategoryById(id);
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/createCategory")
     public categoryModel createCategory(@RequestBody categoryModel category) {
         return categoryService.createCategory(category);
     }
     
+	@CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/updateCategory")
     public categoryModel updateCategory(@RequestBody categoryModel category) {
     	return categoryService.updateCategory(category);
     }
     
+	@CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/deleteCategory/{id}")
     public String deleteCategoryById(@PathVariable Integer id) {
     	return categoryService.deleteCategoryById(id);
